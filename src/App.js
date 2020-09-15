f984fc7f325f60dfcf372bb134baa26c63c806f1
Jon Roberts <kewlguy@gmail.com>
2020-09-15 16:46:05 -0600

    displaying flashcard via Flashcard class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Container, Header, } from "semantic-ui-react"; 
-
+import Flashcards from "./Flashcards";
 class App extends Component {
+state = {
+  flashcards: [
+    { id: 1, question: "What is the Matrix?", answer: "follow the white rabbit", },
+    { id: 2, question: "What is the meaning of life", answer: "The answer is 42", },
+    { id: 3, question: "Question this", answer: "Answer this", },
+  ]
+}
+
   render() {
     return (
       <Container>
         <Header as="h1">Flash Cards</Header>
+        <Flashcards flashcards={this.state.flashcards} />
       </Container>
     );
   }
---
+++ src/Flashcards.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Flashcards = ({ flashcards }) => (
+    <div>
+            
+
+    
+    <h1>Cards:</h1>
+    
+        {flashcards.map(card => (
+    <div>
+        {card.id}<br />
+        {card.question} <br />
+                {card.answer} <br />
+                </div>
+    ))
+    }
+    </div>
+
+    )
+    
+
+export default Flashcards;
