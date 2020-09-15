490a4960b98411905d70b868852aec54cdf09204
neomort <kyle@kyleadams.com>
2020-09-15 15:28:49 -0600

    help me

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Container, Header, } from "semantic-ui-react"; 
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header as="h1">Flash Cards</Header>
+      </Container>
+    );
+  }
+}
+
+export default App;
